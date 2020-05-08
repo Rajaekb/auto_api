41daaee18fdb36d73b60f487175d436320c0de75
Rajaekb <kobirajae@gmail.com>
2020-05-08 12:19:16 +0000

    first commit for api

---
+++ database/migrations/2020_05_06_100215_create_annonces_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnoncesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('Type');
+            $table->boolean('active');
+            $table->foreignId('user_id')->constrained();           
+            $table->integer('nombre_de_vue');
+            $table->timestamps();
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annonces');
+    }
+}
